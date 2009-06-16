bd3a26767879249a37f7b8ba5663159c093d53af
crucialfelix <crucialfelix@ece9b796-0735-11de-b17c-0f1ef23b492c>
2009-06-16 16:53:08 +0000

    adding setup.py

---
+++ ajax_select/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Ajax Select',
+      version='1.0',
+      description='Django-jQuery jQuery-powered auto-complete fields for ForeignKey and ManyToMany fields',
+      author='Crucial Felix',
+      author_email='crucialfelix@gmail.com',
+      url='http://code.google.com/p/django-ajax-selects/',
+      packages=['ajax_select', ],
+     )
