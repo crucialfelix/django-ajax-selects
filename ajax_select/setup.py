8466a67af18d6b7fb7e7019693282e3b0c6fa033
crucialfelix <crucialfelix@ece9b796-0735-11de-b17c-0f1ef23b492c>
2009-10-02 11:27:57 +0000

    fixing setup.py to include data files

--- ajax_select/setup.py
+++ ajax_select/setup.py
@@ -4,9 +4,10 @@ from distutils.core import setup
 
 setup(name='Ajax Select',
       version='1.0',
-      description='Django-jQuery jQuery-powered auto-complete fields for ForeignKey and ManyToMany fields',
-      author='Crucial Felix',
+      description='jQuery-powered auto-complete fields for ForeignKey and ManyToMany fields',
+      author='crucialfelix',
       author_email='crucialfelix@gmail.com',
       url='http://code.google.com/p/django-ajax-selects/',
       packages=['ajax_select', ],
-     )
+      include_package_data = True,    # include everything in source control
+)
