b05c9a7a5bd6cb4d382ef246b7682ef013efefe3
crucialfelix <crucialfelix@gmail.com>
2012-01-14 22:37:27 +0100

    adds an example of using a simple AutoComplete outside of the admin

--- example/urls.py
+++ example/urls.py
@@ -6,6 +6,7 @@ from ajax_select import urls as ajax_select_urls
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^search_form',  view='views.search_form',name='search_form'),
     (r'^admin/lookups/', include(ajax_select_urls)),
     (r'^admin/', include(admin.site.urls)),
 )
