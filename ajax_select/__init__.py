ae4b702348f40d5998c521d5cd88347178a988a8
Ceesjan Luiten <git_development@qtea.nl>
2012-03-08 12:20:12 +0100

    Implemented option for AutoFocus

--- ajax_select/__init__.py
+++ ajax_select/__init__.py
@@ -19,6 +19,7 @@ class LookupChannel(object):
     
     model = None
     min_length = 1
+    auto_focus = True
     
     def get_query(self,q,request):
         """ return a query set searching for the query string q 
