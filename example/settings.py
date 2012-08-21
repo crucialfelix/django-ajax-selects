12650d81f71475a3d1a6a1bffbaf0de7a65334b0
crucialfelix <crucialfelix@gmail.com>
2012-08-21 17:10:08 +0200

    example app: add some setting for testing translation issues

--- example/settings.py
+++ example/settings.py
@@ -100,11 +100,15 @@ TIME_ZONE = 'America/Chicago'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
+# for testing translations
+# LANGUAGE_CODE = 'de-at'
+
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = False
+USE_I18N = True
+
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
