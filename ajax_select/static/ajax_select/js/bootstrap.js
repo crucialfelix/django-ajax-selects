483e48862ad23942315548cf828f45a3319c542f
crucialfelix <crucialfelix@gmail.com>
2013-04-02 02:58:54 +0200

    bumping default jquery to 1.9.1, jquery-ui to 1.8.24 jquery-ui versions after that break

--- ajax_select/static/ajax_select/js/bootstrap.js
+++ ajax_select/static/ajax_select/js/bootstrap.js
@@ -4,10 +4,10 @@ if (typeof jQuery === 'undefined') {
 	try { // use django admins
 		jQuery=django.jQuery;
 	} catch(err) {
-		document.write('<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"><\/script>');
+		document.write('<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"><\/script>');
 	}
 }
 if(typeof jQuery === 'undefined' || (typeof jQuery.ui === 'undefined')) {
-	document.write('<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"><\/script>');
-	document.write('<link type="text/css" rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/smoothness/jquery-ui.css" />');
+	document.write('<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js"><\/script>');
+	document.write('<link type="text/css" rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/themes/smoothness/jquery-ui.css" />');
 }
