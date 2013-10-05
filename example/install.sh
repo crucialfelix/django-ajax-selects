cad25871838315ac5622fbcebf86fdcaccc3ea92
crucialfelix <crucialfelix@gmail.com>
2013-10-05 19:00:27 +0200

    example app: better install.sh script

--- example/install.sh
+++ example/install.sh
@@ -16,15 +16,21 @@ else
     pip install django
 fi
 
-echo "Symlinking ajax_select into this app directory"
-ln -s ../ajax_select/ ./ajax_select
-
 echo "Creating a sqllite database"
 ./manage.py syncdb
 
-echo "run: ./manage.py runserver"
+if [ ! -d ./ajax_select ]; then
+	echo "\nSymlinking ajax_select into this app directory"
+	ln -s ../ajax_select/ ./ajax_select
+fi
+
+echo "\ntype 'source AJAXSELECTS/bin/activate' to activate the virtualenv"
+
+echo '\ncreate an admin account:'
+echo './manage.py createsuperuser'
+
+echo "\nrun: ./manage.py runserver"
 echo "and visit http://127.0.0.1:8000/admin/"
-echo "type 'deactivate' to close the virtualenv or just close the shell"
-echo "type 'source AJAXSELECTS/bin/activate' to re-activate the virtualenv"
+echo "\ntype 'deactivate' to close the virtualenv or just close the shell"
 
 exit 0
