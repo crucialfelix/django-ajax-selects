2bbfaacb829e72efe48f9e84133e292b4d8086fb
crucialfelix <crucialfelix@gmail.com>
2013-10-11 21:26:59 +0200

    fix bootstrap.js: scripts inserted with document.write are not loaded until after the script block closes

--- ajax_select/static/ajax_select/js/bootstrap.js
+++ ajax_select/static/ajax_select/js/bootstrap.js
@@ -1,9 +1,7 @@
 // load jquery and jquery-ui if needed
 // into window.jQuery
 if (typeof window.jQuery === 'undefined') {
-	document.write('<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"><\/script>');
-}
-if(typeof window.jQuery.ui === 'undefined') {
-	document.write('<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js"><\/script>');
-	document.write('<link type="text/css" rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/themes/smoothness/jquery-ui.css" />');
+	document.write('<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"><\/script><script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js"><\/script><link type="text/css" rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/themes/smoothness/jquery-ui.css" />');
+} else if(typeof window.jQuery.ui === 'undefined') {
+	document.write('<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/jquery-ui.min.js"><\/script><link type="text/css" rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.24/themes/smoothness/jquery-ui.css" />');
 }
