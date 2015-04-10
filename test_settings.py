b4d2aa26e0f33f1735ea8e3da932f38705418470
Chris Sattinger <crucialfelix@gmail.com>
2015-04-10 13:05:47 +0200

    add testing with tox and travis for django 1.5 - 1.8, python 2.7
    3.3 matrix still to be added

---
+++ test_settings.py
@@ -0,0 +1,25 @@
+DEBUG = True
+USE_TZ = True
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+    }
+}
+ROOT_URLCONF = "ajax_select.urls"
+INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sites",
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.admin',
+    'django.contrib.staticfiles',
+    "ajax_select",
+]
+SITE_ID = 1
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware'
+)
+SECRET_KEY = 'inyd5fc5pymlsv@hwoc5+3_6*cm0erlxzv6i-wl0jm_kt-6rp9'
