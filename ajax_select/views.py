37ab3e29019321d02d2789a4fb24cfdcd2597775
Villiers Strauss <villiers.strauss@logimeter.com>
2015-04-17 16:40:45 +0200

    ajax_lookup should respond with content type `application/json`, not `application/javascript`

--- ajax_select/views.py
+++ ajax_select/views.py
@@ -44,7 +44,7 @@ def ajax_lookup(request, channel):
         } for item in instances
     ])
 
-    return HttpResponse(results, content_type='application/javascript')
+    return HttpResponse(results, content_type='application/json')
 
 
 def add_popup(request, app_label, model):
