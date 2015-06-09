1bbc639bdb1844ea3e6337b30bded841cdc34e6b
Amichai Schreiber <amichai@workey.co>
2015-06-09 10:42:17 +0300

    no bootstrap -> assume jquery already loaded; avoid jquery conflict

--- ajax_select/static/ajax_select/js/bootstrap.js
+++ ajax_select/static/ajax_select/js/bootstrap.js
@@ -1,13 +1,1 @@
-// load jquery and jquery-ui if needed
-// into window.jQuery
-if (typeof jQuery === 'undefined') {
-	try { // use django admins
-		jQuery=django.jQuery;
-	} catch(err) {
-		document.write('<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"><\/script>');
-	}
-}
-if(typeof jQuery === 'undefined' || (typeof jQuery.ui === 'undefined')) {
-	document.write('<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"><\/script>');
-	document.write('<link type="text/css" rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/smoothness/jquery-ui.css" />');
-}
+$jq16 = jQuery.noConflict(true);
