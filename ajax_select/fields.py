97e7a5f9ef122cef324ecd17ac522d530bb48869
Amichai Schreiber <amichai@workey.co>
2015-06-09 10:42:00 +0300

    django 1.7

--- ajax_select/fields.py
+++ ajax_select/fields.py
@@ -4,7 +4,7 @@ from django import forms
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
-from django.forms.util import flatatt
+from django.forms.utils import flatatt
 from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
