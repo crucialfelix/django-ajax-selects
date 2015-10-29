df36f6303418cb8be3de082d5703c36d15193596
crucialfelix <crucialfelix@gmail.com>
2015-10-29 11:10:37 +0100

    rename sites.py -> registry.py move get_lookup into the LookupChannelRegistry move decorator into registery.py

--- ajax_select/__init__.py
+++ ajax_select/__init__.py
@@ -20,60 +20,6 @@ from .sites import site
 from .decorators import register  # noqa
 
 
-# -----------------------   private  --------------------------------------------- #
-
-def get_lookup(channel):
-    """ find the lookup class for the named channel.  this is used internally """
-    try:
-        lookup_label = site._registry[channel]
-    except KeyError:
-        raise ImproperlyConfigured("settings.AJAX_LOOKUP_CHANNELS not configured correctly for %(channel)r "
-                                   "or autodiscovery could not locate %(channel)r" % {'channel': channel})
-
-    if isinstance(lookup_label, dict):
-        # 'channel' : dict(model='app.model', search_field='title' )
-        #  generate a simple channel dynamically
-        return make_channel(lookup_label['model'], lookup_label['search_field'])
-    else:  # a tuple
-        # 'channel' : ('app.module','LookupClass')
-        #  from app.module load LookupClass and instantiate
-        lookup_module = __import__(lookup_label[0], {}, {}, [''])
-        lookup_class = getattr(lookup_module, lookup_label[1])
-
-        # monkeypatch older lookup classes till 1.3
-        if not hasattr(lookup_class, 'format_match'):
-            setattr(lookup_class, 'format_match',
-                getattr(lookup_class, 'format_item',
-                    lambda self, obj: force_text(obj)))
-        if not hasattr(lookup_class, 'format_item_display'):
-            setattr(lookup_class, 'format_item_display',
-                getattr(lookup_class, 'format_item',
-                    lambda self, obj: force_text(obj)))
-        if not hasattr(lookup_class, 'get_result'):
-            setattr(lookup_class, 'get_result',
-                getattr(lookup_class, 'format_result',
-                    lambda self, obj: force_text(obj)))
-
-        return lookup_class()
-
-
-def make_channel(app_model, arg_search_field):
-    """ used in get_lookup
-            app_model :   app_name.model_name
-            search_field :  the field to search against and to display in search results
-    """
-    from django.db import models
-    app_label, model_name = app_model.split(".")
-    themodel = models.get_model(app_label, model_name)
-
-    class MadeLookupChannel(LookupChannel):
-
-        model = themodel
-        search_field = arg_search_field
-
-    return MadeLookupChannel()
-
-
 def autodiscover():
     try:
         from django.utils.module_loading import autodiscover_modules
