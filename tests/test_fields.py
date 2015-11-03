e42b9cebddc6649dde0d5f3f4b4be21de992f5e4
crucialfelix <crucialfelix@gmail.com>
2015-11-03 13:42:04 +0100

    tests: render each widget

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -2,9 +2,32 @@ from django.test import TestCase
 from ajax_select import fields
 
 
-class TestAjaxSelectAutoCompleteSelectWidget(TestCase):
+class WidgetTester(TestCase):
+    pass
+
+
+class TestAutoCompleteSelectWidget(WidgetTester):
 
     def test_render(self):
-        channel = None
+        channel = 'book'
         widget = fields.AutoCompleteSelectWidget(channel)
-        widget
+        out = widget.render('book', None)
+        self.assertTrue('autocompleteselect' in out)
+
+
+class TestAutoCompleteSelectMultipleWidget(WidgetTester):
+
+    def test_render(self):
+        channel = 'book'
+        widget = fields.AutoCompleteSelectMultipleWidget(channel)
+        out = widget.render('book', None)
+        self.assertTrue('autocompleteselectmultiple' in out)
+
+
+class TestAutoCompleteWidget(WidgetTester):
+
+    def test_render(self):
+        channel = 'book'
+        widget = fields.AutoCompleteWidget(channel)
+        out = widget.render('book', None)
+        self.assertTrue('autocomplete' in out)
