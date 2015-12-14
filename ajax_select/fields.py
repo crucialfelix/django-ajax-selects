8915805236429f9a80d9e4a453ba2ec97ff918b9
Amichai Schreiber <amichai@workey.co>
2015-12-14 21:25:14 +0200

    allow non-integer IDs in AutoCompleteField

--- ajax_select/fields.py
+++ ajax_select/fields.py
@@ -89,11 +89,7 @@ class AutoCompleteSelectWidget(forms.widgets.TextInput):
 
     def value_from_datadict(self, data, files, name):
 
-        got = data.get(name, None)
-        if got:
-            return long(got)
-        else:
-            return None
+        return data.get(name, None) or None
 
     def id_for_label(self, id_):
         return '%s_text' % id_
