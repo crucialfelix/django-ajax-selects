crucialfelix/django-ajax-selects

commit a37766905911b4d529b13dbcf384eb6e2f0d8a71

fix #123 - changed_data always includes AutoComplete fields
it was because the fields are CharFields so they return pk of u'1'
but the initial value is 1

ajax_select/fields.py
@@ -139,6 +139,10 @@ class AutoCompleteSelectField(forms.fields.CharField):
     def check_can_add(self, user, model):
         _check_can_add(self, user, model)
 
+    def has_changed(self, initial_value, data_value):
+        # 1 vs u'1'
+        return text_type(initial_value) != text_type(data_value)
+
 
 ####################################################################################
 
@@ -285,6 +289,11 @@ class AutoCompleteSelectMultipleField(forms.fields.CharField):
     def check_can_add(self, user, model):
         _check_can_add(self, user, model)
 
+    def has_changed(self, initial_value, data_value):
+        # [1, 2] vs [u'1', u'2']
+        ivs = [text_type(v) for v in initial_value]
+        dvs = [text_type(v) for v in data_value]
+        return ivs != dvs
 
 ####################################################################################
 

tests/test_fields.py
@@ -2,11 +2,7 @@ from django.test import TestCase
 from ajax_select import fields
 
 
-class WidgetTester(TestCase):
-    pass
-
-
-class TestAutoCompleteSelectWidget(WidgetTester):
+class TestAutoCompleteSelectWidget(TestCase):
 
     def test_render(self):
         channel = 'book'
@@ -15,7 +11,7 @@ class TestAutoCompleteSelectWidget(WidgetTester):
         self.assertTrue('autocompleteselect' in out)
 
 
-class TestAutoCompleteSelectMultipleWidget(WidgetTester):
+class TestAutoCompleteSelectMultipleWidget(TestCase):
 
     def test_render(self):
         channel = 'book'
@@ -24,10 +20,28 @@ class TestAutoCompleteSelectMultipleWidget(WidgetTester):
         self.assertTrue('autocompleteselectmultiple' in out)
 
 
-class TestAutoCompleteWidget(WidgetTester):
+class TestAutoCompleteWidget(TestCase):
 
     def test_render(self):
         channel = 'book'
         widget = fields.AutoCompleteWidget(channel)
         out = widget.render('book', None)
         self.assertTrue('autocomplete' in out)
+
+
+class TestAutoCompleteSelectField(TestCase):
+
+    def test_has_changed(self):
+        field = fields.AutoCompleteSelectField('book')
+        self.assertFalse(field.has_changed(1, '1'))
+        self.assertFalse(field.has_changed('abc', 'abc'))
+        self.assertTrue(field.has_changed(1, '2'))
+
+
+class TestAutoCompleteSelectMultipleField(TestCase):
+
+    def test_has_changed(self):
+        field = fields.AutoCompleteSelectMultipleField('book')
+        self.assertFalse(field.has_changed([1], ['1']))
+        self.assertFalse(field.has_changed(['abc'], ['abc']))
+        self.assertTrue(field.has_changed([1], ['2']))

commit 39bb8b2c5e2f2cd22049c0b6434f29580c2e8fa5

Fixes issue introduced by a377669 and related issue #142, by using a similar solution that `django.forms.models.ModelChoiceField.has_changed` uses.

ajax_select/fields.py
@@ -140,8 +140,10 @@ class AutoCompleteSelectField(forms.fields.CharField):
     def check_can_add(self, user, model):
         _check_can_add(self, user, model)
 
-    def has_changed(self, initial_value, data_value):
+    def has_changed(self, initial, data):
         # 1 vs u'1'
+        initial_value = initial if initial is not None else ''
+        data_value = data if data is not None else ''
         return text_type(initial_value) != text_type(data_value)
 
 