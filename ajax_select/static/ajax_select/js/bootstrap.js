6171c58fc39acf7ad5f343071b7633c04b5e8f41
tdphillips <troy@apps.directemployers.org>
2016-10-12 14:24:11 -0400

    Prefer document.createElement to document.write when adding resources

--- ajax_select/static/ajax_select/js/bootstrap.js
+++ ajax_select/static/ajax_select/js/bootstrap.js
@@ -1,7 +1,21 @@
 // load jquery and jquery-ui if needed
 // into window.jQuery
+var firstScript = document.getElementsByTagName('script')[0];
 if (typeof window.jQuery === 'undefined') {
-  document.write('<script type="text/javascript"  src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"><\/script><script type="text/javascript"  src="//code.jquery.com/ui/1.10.3/jquery-ui.js"><\/script><link type="text/css" rel="stylesheet" href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />');
-} else if(typeof window.jQuery.ui === 'undefined' || typeof window.jQuery.ui.autocomplete === 'undefined') {
-  document.write('<script type="text/javascript"  src="//code.jquery.com/ui/1.10.3/jquery-ui.js"><\/script><link type="text/css" rel="stylesheet" href="//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />');
+  var newScript = document.createElement('script');
+  newScript.type = 'text/javascript';
+  newScript.src = '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js';
+  firstScript.parentNode.insertBefore(newScript, firstScript);
+}
+if (typeof window.jQuery === 'undefined' || typeof window.jQuery.ui === 'undefined' || typeof window.jQuery.ui.autocomplete === 'undefined') {
+  var newScript = document.createElement('script');
+  newScript.type = 'text/javascript';
+  newScript.src = '//code.jquery.com/ui/1.10.3/jquery-ui.js';
+  firstScript.parentNode.insertBefore(newScript, firstScript);
+
+  var newLink = document.createElement('link');
+  newLink.type = 'text/css';
+  newLink.rel = 'stylesheet';
+  newLink.href = "//code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css"
+  firstScript.parentNode.insertBefore(newLink, firstScript);
 }
