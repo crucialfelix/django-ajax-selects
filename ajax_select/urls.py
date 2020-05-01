38ad6b4b60ab1500a141549fe9997704ce2af3ef
Ryan Jarvis <ryan@technoloft.com>
2020-05-01 16:25:08 -0700

    Add app_name for Django >= 2.0

--- ajax_select/urls.py
+++ ajax_select/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from ajax_select import views
 
+app_name = 'ajax_lookup'
+
 urlpatterns = [
     url(r'^ajax_lookup/(?P<channel>[-\w]+)$',
         views.ajax_lookup,
