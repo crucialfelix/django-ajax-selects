205068d2e0c23a78c3ae9edfc1979bc8341ceeb9
Thomas Langenskiöld <tlangens@gmail.com>
2020-11-21 13:37:01 +0200

    Remove leftover ')'
    Addresses https://github.com/crucialfelix/django-ajax-selects/issues/280

--- ajax_select/urls.py
+++ ajax_select/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from ajax_select import views
 
 urlpatterns = [
-    path('ajax_lookup/<channel>)',
+    path('ajax_lookup/<channel>',
          views.ajax_lookup,
          name='ajax_lookup')
 ]
